ee9e8585f4c057350cc7395bfe6a11aab46c9209
Parsa Mohammadian <parsa2820@gmail.com>
2022-07-31 21:49:08 +0430

    Add id to ticket

--- server/models/support/ticket.py
+++ server/models/support/ticket.py
@@ -15,22 +15,23 @@ class Reply:
 
 
 class Ticket:
-    def __init__(self, account: Account, message: str):
+    def __init__(self, id, account: Account, message: str):
+        self.id = id
         self.account: Account = account
         self.message: str = message
         self.status = "new"
         self.replies: List[Reply] = []
 
     def from_json(json_data: dict):
-        ticket = Ticket(Account.from_json(
-            json_data["account"]), json_data["message"])
+        ticket = Ticket(Account.from_json(json_data["id"],
+                                          json_data["account"]), json_data["message"])
         ticket.status = json_data["status"]
         ticket.replies = [Reply.from_json(reply)
                           for reply in json_data["replies"]]
         return ticket
 
     def __str__(self):
-        return "Ticket from {}: {}".format(str(self.account), self.message) \
+        return "Ticket {} from {}: {}".format(id, str(self.account), self.message) \
             + "\nStatus: {}".format(self.status) \
             + "\nReplies:\n\t" \
             + "\n\t".join([str(reply) for reply in self.replies])
