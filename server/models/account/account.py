0c22249d62d5efc592af7a60aadd3ee31bc7045d
saaz742 <azarnooshsa@gmail.com>
2022-07-31 16:52:29 +0430

    add command processor and accounts

--- server/models/account/account.py
+++ server/models/account/account.py
@@ -1,1 +1,14 @@
-# TODO SARA
+from abc import ABC, abstractmethod
+import re
+
+class Account(ABC):
+
+    all_accounts = []
+
+    def __init__(self, values):
+        self.username = values[0]
+        self.password = values[1]
+        self.online = False
+        Account.all_accounts.append(self)
+
+
--- server/models/account/manager.py
+++ server/models/account/manager.py
@@ -1,1 +1,8 @@
-# TODO SARA
+
+class Manager():
+
+    def __init__(self, values):
+         super().__init__(values)
+
+    def add_manager(self, values):
+        Manager(values)
