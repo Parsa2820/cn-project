cef2bff3ab0bf176e11868fedf2fcf3928089ae5
Parsa Mohammadian <parsa2820@gmail.com>
2022-08-07 09:52:41 +0430

    Implement client menu using server help function

--- client/main.py
+++ client/main.py
@@ -1,15 +1,23 @@
+from re import A
 import socket
+import os
 
 PORT = 2820
 USERNAME = "_"
 PASSWORD = "_"
+LOGGED_IN = False 
+MENU = []
+PROMPT = "\nEnter menu item number: "
 
 
 def run() -> None:
+    init_menus()
     while not login():
         print("Login failed. Please retry")
     print("Login successfull")
-    command = input(">> ")
+    _ = input("\nPress enter to continue")
+    command = generate_command()
+    print(command)
     while command != "exit":
         if command == "logout":
             logout()
@@ -20,12 +28,54 @@ def run() -> None:
         response = client.recv(1024).decode()
         print(response)
         client.close()
-        command = input(">> ")
+        _ = input("\nPress enter to continue")
+        command = generate_command()
+        print(command)
+
+
+def init_menus():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', PORT))
+    client.send("help".encode())
+    response = client.recv(1024).decode()
+    entries = response.split('\n')
+    entries = [e.split(' ') for e in entries]
+    global MENU
+    MENU = [(e[0], e[1:]) for e in entries]
+    
+
+def print_menu() -> None:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for idx, entry in enumerate(MENU):
+        print(f"{idx+1}-{entry[0]}")
+
+
+def get_command_parameters(parameters: list) -> list:
+    return [input(f"{parameter}: ") for parameter in parameters]
+
+
+def generate_command() -> str:
+    print_menu()
+    action = input(PROMPT)
+    try:
+        action = int(action) - 1
+    except:
+        return action
+    params_value = []
+    for param in MENU[action][1]:
+        if not MENU[action][0].startswith("register"):
+            if param == "username":
+                params_value.append(USERNAME)
+            elif param == "password":
+                params_value.append(PASSWORD)
+        else:
+            params_value.append(input(f"{param}: "))
+    return f"{MENU[action][0]} {USERNAME} {PASSWORD} {' '.join(params_value)}"
 
 
 def login() -> bool:
     print("1-Login\n2-Continue as a guest")
-    login = input() == "1"
+    login = input(PROMPT) == "1"
     if login:
         username = input("Username: ")
         password = input("Password: ")
@@ -37,8 +87,10 @@ def login() -> bool:
         if "success" in response:
             global USERNAME
             global PASSWORD
+            global LOGGED_IN
             USERNAME = username
             PASSWORD = password
+            LOGGED_IN = True
             return True
         return False
     return True
@@ -47,8 +99,10 @@ def login() -> bool:
 def logout() -> None:
     global USERNAME
     global PASSWORD
+    global LOGGED_IN
     USERNAME = "_"
     PASSWORD = "_"
+    LOGGED_IN = False
 
 
 if __name__ == '__main__':
