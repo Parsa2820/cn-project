13a898d0661f9d27c9b16781c6445685f8c84eff
saaz742 <azarnooshsa@gmail.com>
2022-08-08 23:52:15 +0430

    fix get request

--- server/models/account/request.py
+++ server/models/account/request.py
@@ -1,14 +1,14 @@
 class Request:
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str, password: str, status: str = "pending"):
         """
         status -> pending, accepted, rejected
         """
         self.username: str = username
         self.password: str = password
-        self.status: str = "pending"
+        self.status: str = status
 
     def from_json(json_data: dict):
-        return Request(json_data["username"], json_data["password"])
+        return Request(json_data["username"], json_data["password"], json_data["status"])
 
     def __str__(self):
         return "{} {}".format(self.status, self.username)
